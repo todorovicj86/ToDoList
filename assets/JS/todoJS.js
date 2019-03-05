f1b7ac4831c511bc989efb21e9b79cc41573ab9f
todorovicj86 <44524274+todorovicj86@users.noreply.github.com>
2019-03-05 14:51:08 +0100

    counting completed and active todos

--- assets/JS/todoJS.js
+++ assets/JS/todoJS.js
@@ -5,12 +5,25 @@
 // 	alert("not good!");
 // }
 
+//define variables for counting
+var deleted = 0;
+var complete;
+var active;
+
+//displays all the todos
+$("#totalActive").text($("li.newToDo").length);
 
 //when icon delete is clicked, remove the item from the list
 $(document).on("click","i.delete", function(){
 	//removing the item with fadeOut
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
+		//count removed todos
+		deleted ++;
+		//writes active toods
+	    $("#totalActive").text($("li.newToDo").length - $("span.completed").length);
+	    //writes total completed(removed + completed)
+	    $("#totalComp").text($("span.completed").length + deleted);
 	});
 	update();
 });
@@ -18,6 +31,15 @@ $(document).on("click","i.delete", function(){
 //when element is clicked, cross it, but leave it visible
 $(document).on("click","span", function(){
 	$(this).toggleClass("deleted");
+	//count completed todos
+	complete = $("span.completed").length;
+		 
+	// counts active todos
+	active = $("li.newToDo").length - complete;
+	
+	//write number of completed and active todos
+	$("#totalComp").text($("span.completed").length + deleted);
+	$("#totalActive").text(active);
 });
 
 //when press enter, add new item to the list, at the bottom
@@ -30,7 +52,15 @@ $("input").on("keypress", function(event){
 		$("ul").append(newLI);
 		//adding the text to last span in todo list
 		$("span:last").text(" " + newItem);
-		reset();
+
+		//changes active todos when new is added
+		$("#totalActive").text($("li.newToDo").length - $("span.completed").length);
+		
+		 //resets input text
+		 reset();
+
+		 //if all is completed, restart completed to 0
+		 resetCompleted();
 	};
 
 });
@@ -48,4 +78,13 @@ function reset(){
 //update background colors of lis
 function update(){
 	$("ul li:nth-child(3)").css("background","#DDE8FB");
+}
+
+//resets the completed and deleted to 0, when everyting is done
+function resetCompleted(){
+	if ($("span.completed").length === 0){
+		$("span.completed").length = 0;
+		deleted = 0;
+		$("#totalComp").text($("span.completed").length + deleted);
+	}
 }
