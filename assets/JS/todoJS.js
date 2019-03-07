c5c939875918afc994e773a1d4804112df1b64b9
todorovicj86 <44524274+todorovicj86@users.noreply.github.com>
2019-03-07 13:38:04 +0100

    added active, completed and all filters
    When in active mode, only active todos are shown, and possibility to add new. The color of rows is adjusted.
    When in completed mode, only completed are presentd. Not possible to add new. Color of rows is adjusted.
    When in all mode, all todos are presented. Possible to add new todos.

--- assets/JS/todoJS.js
+++ assets/JS/todoJS.js
@@ -62,6 +62,8 @@ $("input").on("keypress", function(event){
 
 		 //if all is completed, restart completed to 0
 		 resetCompleted();
+		 //resets the color when new todo is added
+		 resetColor();
 	};
 
 });
@@ -70,6 +72,111 @@ $("input").on("keypress", function(event){
 $("#plus").on("click", function(){
 	$("li:nth-child(2)").slideToggle("slow");
 });
+
+//filter active, completed and all todos
+$(document).on("click","input[type='checkbox']", function(){
+		
+	var active_todo = $("input[value='activeTodo']");
+	var compl_todo = $("input[value='completeTodo']");
+	var all_todo = $("input[value='allTodo']");
+	
+	//when checked
+	if ($(this).is(":checked")){
+		
+		//set the checked to true
+		$(this).prop("checked", true);
+		
+		//if active is clciked and checked
+		if($(this).val() === "activeTodo"){
+			//show active
+			$("span.active").parent().show();
+			
+			//hide existing completed
+			$("span.completed").parent().hide();					
+			//show add new input
+			$("li:nth-child(2)").show();
+			//uncheck completed
+			compl_todo.prop("checked", false);
+			//uncheck all
+			all_todo.prop("checked", false);
+
+			//if completed is checked
+		} else if($(this).val() === "completeTodo"){
+			//hide active
+			$("span.active").parent().hide();
+			//show completed
+			$("span.completed").parent().show();
+			//hide add new in completed mode
+			$("li:nth-child(2)").hide();
+			//suncheck active and all
+			active_todo.prop("checked", false);
+			all_todo.prop("checked", false);
+			
+			//if all is checked
+		}else if($(this).val() === "allTodo"){
+			//show active and completed
+			$("span.completed").parent().show();
+			$("span.active").parent().show();
+			//show add new input
+			$("li:nth-child(2)").show();
+			//unckeck active and completed
+			active_todo.prop("checked", false);
+			compl_todo.prop("checked", false);
+
+		}
+	//if active and completed are unchecked
+	}else if($(this).not(":checked")){
+		//set all as checked
+		all_todo.prop("checked", true);
+		//show both, active and completed
+		$("span.completed").parent().show();
+		$("span.active").parent().show()
+	}
+	//reset the colors
+	resetColor();
+});
+
+//click event on new todos when in active or completed mode
+$(document).on("click",".todo", function(){
+	//if active or completed mode is checked
+	if($("input[name='active'").is(":checked") || $("input[name='complete'").is(":checked")){
+		//remove the todo when completed or (un)completed
+		$(this).parent().fadeOut();
+
+		//reset the color of all todos to be the same
+		$(".newToDo").css("background","#DDE8FB");
+		//set the odd .active to be the diffrent color - when in actvie mode
+		$(".todo.active:odd").parent().css("background","#77E8D6");
+		//set the odd .completed to be diffrent color-when in completed mode
+		$(".todo.completed:odd").parent().css("background","#77E8D6");
+	}
+
+});
+
+//resets colors of todo's, when in different modes
+function resetColor(){
+	//if in mode all
+ 	if($("input[name='all'").is(":checked")){
+ 		//set all to be the same color
+		$(".newToDo").css("background","#DDE8FB");
+		//set the odd rows to be the different color
+		$(".newToDo:odd").css("background","#77E8D6");
+	}
+	//if in mode completed
+	if( $("input[name='complete'").is(":checked")){
+		//set all to be the same color
+		$(".newToDo").css("background","#DDE8FB");
+		//set the odd completed rows to be the different color
+		$(".todo.completed:odd").parent().css("background","#77E8D6");
+	}
+	//if in mode active
+	if($("input[name='active'").is(":checked")){
+		//set all  to be the same color
+		$(".newToDo").css("background","#DDE8FB");
+		//set the odd active rows to be the different color
+		$(".todo.active:odd").parent().css("background","#77E8D6");
+	}
+}
 
 //resets the completed and deleted to 0, when everyting is done
 function resetCompleted(){
